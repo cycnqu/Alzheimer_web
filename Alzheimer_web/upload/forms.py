7c8ec2d209ac819d6cd7575e03435b72b4dc9a62
Rick Chen <rick20001010@gmail.com>
2022-04-11 10:36:38 +0800

    fixed preview upload pic

--- Alzheimer_web/upload/forms.py
+++ Alzheimer_web/upload/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from upload.models import * 
-from django.contrib.admin.decorators import display
-from django.template.loader import get_template
-
 class Upload_Image_Form(forms.ModelForm):
     class Meta:
         model = Upload_Image
         fields = ['image','date']
-        
+        #widgets = {
+        #    'image':forms.FileInput(attrs={'class':'imagecontrol'})
+        #}
