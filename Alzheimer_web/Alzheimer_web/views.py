ad1cc88439c39a5f4ef09dff25c93be82de5e97e
brian891005 <kenmill1005@gmail.com>
2022-04-12 05:37:03 +0800

    add contact form

--- Alzheimer_web/Alzheimer_web/views.py
+++ Alzheimer_web/Alzheimer_web/views.py
@@ -5,13 +5,4 @@ from login.form import *
 
 # start django 
 def home(request):
-    form = CustomerModelForm()
-    if request.method == "POST":
-        form = CustomerModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/customers')
-    context = {
-        'form': form
-    }
     return render(request, 'index.html')
