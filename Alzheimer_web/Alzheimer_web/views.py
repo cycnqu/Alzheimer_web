4f8053ce3047bac7c3f2a3b6f97a1d7d90cbc73e
Rick Chen <rick20001010@gmail.com>
2022-05-19 23:39:19 +0800

    20220519

--- Alzheimer_web/Alzheimer_web/views.py
+++ Alzheimer_web/Alzheimer_web/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 from upload.forms import *
 # start django 
 def home(request):
+    # check user's ip who visit our home page
+    user = request.user
+    user_agent = request.META.get('HTTP_USER_AGENT', 'unknown')
+    ip = request.META['REMOTE_ADDR']
+    print(ip)
     return render(request, 'index.html')
